d8471f50d58f827a5f6043f4e6894897da0f9a16
Kritagya Shrestha <kritagyasht@gmail.com>
2026-06-20 22:29:55 +0545

    Add calorie index and goal update routes

--- app/routes/nutrition_routes.py
+++ app/routes/nutrition_routes.py
@@ -7,3 +7,12 @@ def macro_targets_index():
 @login_required
 def macro_targets_save():
     return NutritionController.macro_targets_save()
+@nutrition_bp.route('/calories')
+@login_required
+def calorie_index():
+    return NutritionController.calorie_index()
+
+@nutrition_bp.route('/calories/goal', methods=['POST'])
+@login_required
+def update_goal():
+    return NutritionController.update_goal()
